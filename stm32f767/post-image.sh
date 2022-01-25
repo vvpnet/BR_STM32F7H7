d18e9c40d6a9fcf4030ea3d4dccc5d1406ecc28e
vvpnet <v.v.p.net@mail.ru>
2022-01-25 08:58:44 +0600

    -Выбрана версия ядра 5.10.х -Доработаны скрипты сборки.
    Booting Linux on physical CPU 0x0
    Linux version 5.10.83 (vpopov@ThinkPad-P14s) (11.2.0, GNU ld (GNU Binutils) 2.32)
    CPU: ARMv7-M [411fc270] revision 0 (ARMv7M), cr=00000000
    CPU: PIPT / VIPT nonaliasing data cache, PIPT instruction cache
    OF: fdt: Machine model: STMicroelectronics  STM32F767IGT6-LQFP176 board

--- stm32f767/post-image.sh
+++ stm32f767/post-image.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
 # Deploy image and dtb to ResultDir
 
+ver_uboot="2021.10"
+ver_kernel="5.10.83"
 dir_publish1="${BR2_EXTERNAL_STM32F767_PATH}/stm32f767/ImageLinux"
 dir_publish2="/home/vpopov/minicom/send/linux"
 
 echo "Publish1 Files to" ${dir_publish1}
+
 echo "Copy zImage to " ${dir_publish1}
 cp ${BINARIES_DIR}/zImage ${dir_publish1}
+
 echo "Copy dtb to" ${dir_publish1}
-cp ${BUILD_DIR}/linux-4.14.256/arch/arm/boot/dts/stm32f746-disco.dtb ${dir_publish1}/stm32f767-lqfp176.dtb
+cp ${BUILD_DIR}/linux-${ver_kernel}/arch/arm/boot/dts/stm32f746-disco.dtb ${dir_publish1}/stm32f767-lqfp176.dtb
+
+echo "Copy uboot.bin to" ${dir_publish1}
+cp ${BUILD_DIR}/uboot-${ver_uboot}/u-boot.bin ${dir_publish1}/uboot-stm32f767-lqfp176.bin
+
+echo "Copy spl-uboot.bin to" ${dir_publish1}
+cp ${BUILD_DIR}/uboot-${ver_uboot}/spl/u-boot-spl.bin ${dir_publish1}/spl-uboot-stm32f767-lqfp176.bin
 
 if [ -d ${dir_publish2} ]; then
     echo "Publish2 Files to" ${dir_publish2}
+    
     echo "Copy zImage to " ${dir_publish2}
     cp ${BINARIES_DIR}/zImage ${dir_publish2}
-    echo "Copy dtb to" ${dir_publish1}
-    cp ${BUILD_DIR}/linux-4.14.256/arch/arm/boot/dts/stm32f746-disco.dtb ${dir_publish2}/stm32f767-lqfp176.dtb
+    
+    echo "Copy dtb to" ${dir_publish2}
+    cp ${BUILD_DIR}/linux-${ver_kernel}/arch/arm/boot/dts/stm32f746-disco.dtb ${dir_publish2}/stm32f767-lqfp176.dtb
+    
+    echo "Copy uboot.bin to" ${dir_publish2}
+    cp ${BUILD_DIR}/uboot-${ver_uboot}/u-boot.bin ${dir_publish2}/uboot-stm32f767-lqfp176.bin
+
+    echo "Copy spl-uboot.bin to" ${dir_publish2}
+    cp ${BUILD_DIR}/uboot-${ver_uboot}/spl/u-boot-spl.bin ${dir_publish2}/spl-uboot-stm32f767-lqfp176.bin
 fi
