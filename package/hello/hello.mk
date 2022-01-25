5ebf2302b3255a99ee22d94615ff8fed04f4eb32
Попов Владимир <v.v.p.net@mail.ru>
2022-01-25 11:09:25 +0600

    Update hello.mk

--- package/hello/hello.mk
+++ package/hello/hello.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 HELLO_VERSION = 1.0
-HELLO_SITE = ./package/hello/src
+HELLO_SITE = ../package/hello/src
 HELLO_SITE_METHOD = local
 HELLO_DEPENDENCIES = 
 
